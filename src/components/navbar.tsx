9b02a2f65d6f8b3419b571adee32df601d365ee6
korarit <krtza1@gmail.com>
2024-10-29 12:05:49 +0700

    navbar to compunent

---
+++ src/components/navbar.tsx
@@ -0,0 +1,66 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from 'next/navigation';
+
+
+export default function Navbar() {
+
+
+    const currentPath = usePathname();
+
+    return (
+    <>
+      <div className="h-[50px] w-full flex flex-none">
+        <div className="flex-none h-full w-[256px] flex items-center px-4 border-b border-r border-[#3F3F3F]">
+          <p className="text-[#959595] text-[16px] font-normal">Korarit - CV</p>
+        </div>
+        <Link
+          href="/"
+          className={
+            currentPath === "/"
+              ? "flex-none h-full px-4 flex items-center border-b-4 border-r border-b-[#FFFFFF] border-r-[#3F3F3F] text-[#959595] "
+              : "flex-none h-full flex items-center px-4 border-b border-r border-[#3F3F3F] text-[#959595] hover:text-white"
+          }
+        >
+          <p className="text-[16px] font-normal">Hello.txt</p>
+        </Link>
+        <Link
+          href="/about"
+          className={
+            currentPath === "/about"
+              ? "flex-none h-full px-4 flex items-center border-b-4 border-r border-b-[#FFFFFF] border-r-[#3F3F3F] text-[#959595]"
+              : "flex-none h-full flex items-center px-4 border-b border-r border-[#3F3F3F] text-[#959595] hover:text-white"
+          }
+        >
+          <p className="text-[16px] font-normal">../about</p>
+        </Link>
+        <Link
+          href="/skill"
+          className={
+            currentPath === "/skill"
+              ? "flex-none h-full px-4 flex items-center border-b-4 border-r border-b-[#FFFFFF] border-r-[#3F3F3F] text-[#959595]"
+              : "flex-none h-full flex items-center px-4 border-b border-r border-[#3F3F3F] text-[#959595] hover:text-white"
+          }
+        >
+          <p className="text-[16px] font-normal">../skill</p>
+        </Link>
+        <Link
+          href="/projects"
+          className={
+            currentPath === "/projects"
+              ? "flex-none h-full px-4 flex items-center border-b-4 border-r border-b-[#FFFFFF] border-r-[#3F3F3F] text-[#959595]"
+              : "flex-none h-full flex items-center px-4 border-b border-r border-[#3F3F3F] text-[#959595] hover:text-white"
+          }
+        >
+          <p className="text-[16px] font-normal">../projects</p>
+        </Link>
+
+        <div className="flex-auto h-full flex items-center border-b border-r border-[#3F3F3F]"></div>
+
+        <button className="flex-none w-[148px] xl:w-[10%]  h-full flex items-center justify-center border-b border-r border-[#3F3F3F] text-[#959595] hover:text-white">
+          <p className=" text-[16px] font-normal">Download CV</p>
+        </button>
+      </div>
+    </>
+    );
+}
