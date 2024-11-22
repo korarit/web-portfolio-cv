ab0dd413d628086a27cd7b04914a7ad9342c2ece
korarit <krtza1@gmail.com>
2024-11-23 01:44:23 +0700

    dev for get contact data form db

--- src/app/api/contact_at/get.ts
+++ src/app/api/contact_at/get.ts
@@ -1,19 +1,45 @@
 import { PrismaClient } from '@prisma/client'
-import { count } from 'console'
 
 const prisma = new PrismaClient()
 
-export async function GetContact() {
+export async function getSocial() {
     try {
     const data = await prisma.contact_me.findMany({
         select:{
             name: true,
             icon: true,
             link: true
+        },
+        where: {
+            social: {
+                equals: true
+            }
         }
     })
 
-    return {list_contact: data, count : data.length}
+    return {list: data, count : data.length}
+
+    } catch (error) {
+        throw error
+    }
+}
+
+export async function getProjects() {
+    try {
+    const data = await prisma.contact_me.findMany({
+        select:{
+            name: true,
+            icon: true,
+            link: true
+        },
+        where: {
+            show_project: {
+                equals: true
+            }
+        }
+    })
+
+    return {list: data, count : data.length}
 
     } catch (error) {
         throw error
--- src/app/api/contact_at/route.ts
+++ src/app/api/contact_at/route.ts
@@ -1,16 +1,41 @@
-import { NextResponse } from 'next/server'
+import { NextResponse, NextRequest } from 'next/server'
 
-import { GetContact } from './get'
+import { getSocial, getProjects } from './get'
+import addContantDB from './post'
 
 
-export async function GET() {
+export async function GET(Request: NextRequest) {
     try {
-        const data = await GetContact()
-        return NextResponse.json({
-            success: true,
-            message: 'contact data fetched successfully',
-            data: data
-        }, {status: 200})
+
+        const Social = Request.nextUrl.searchParams.get('social')
+        const ShowProjects = Request.nextUrl.searchParams.get('project')
+
+        if (Social === null && ShowProjects === null) {
+            return NextResponse.json({
+                success: false,
+                message: 'invalid query parameter',
+                error: 'query parameter must be social or project'
+            }, {status: 400})
+        }
+
+        if (Social !== null && Social === 'true') {
+            const data = await getSocial()
+            return NextResponse.json({
+                success: true,
+                message: 'contact data fetched successfully',
+                data: data
+            }, {status: 200})
+        }
+
+        if (ShowProjects !== null && ShowProjects === 'true') {
+            const data = await getProjects()
+            return NextResponse.json({
+                success: true,
+                message: 'contact data fetched successfully',
+                data: data
+            }, {status: 200})
+        }
+        
     } catch (error) {
         return NextResponse.json({
             success: false,
@@ -18,4 +43,34 @@ export async function GET() {
             error: error
         }, {status: 500})
     }    
+}
+
+interface Contact {
+    name: string,
+    link: string,
+    icon: string,
+    social: boolean
+}
+export async function POST(Request: NextRequest) {
+    try {
+        const {name, link, icon, social} = await Request.json()
+        
+        if (!name || !link || !icon || !social) {
+            return NextResponse.json({
+                success: false, message: 'missing required field name, link, icon, social are required',
+            }, {status: 400})
+        }
+
+        const contactData: Contact = {name, link, icon, social}
+
+        const result = await addContantDB(contactData.name, contactData.link, contactData.icon, contactData.social)
+        return NextResponse.json(result, {status: 201})
+
+    } catch (error) {
+        return NextResponse.json({
+            success: false,
+            message: 'failed to add contact',
+            error: error
+        }, {status: 500})
+    }
 }
