966adfcc01908997e1336686fe8c9b1274632da6
korarit <krtza1@gmail.com>
2024-11-25 00:30:05 +0700

    fix skill topic controller return

--- src/app/api/skill/topic/[id]/post.ts
+++ src/app/api/skill/topic/[id]/post.ts
@@ -4,17 +4,21 @@ import { uploadToR2 } from '@/lib/cloudflare'
 
 const prisma = new PrismaClient()
 
-export async function addSkillToTopic(topic_id: number, name: FormDataEntryValue|null, icon: FormDataEntryValue|null): Promise<[boolean, number, {success: boolean, message: string, data?: any}]> {
+export async function addSkillToTopic(
+    topic_id: number, 
+    name: FormDataEntryValue|null, 
+    icon: FormDataEntryValue|null
+): Promise<[number, {success: boolean, message: string, data?: any}]> {
     try{
         if (!name ||  !icon) {
-            return [false, 400,{success: false, message: 'name and icon is required'}]
+            return [400,{success: false, message: 'name and icon is required'}]
         }
         if (!icon) {
-            return [false, 400,{success: false, message: 'icon is required'}]
+            return [400,{success: false, message: 'icon is required'}]
         }
 
         if (!(icon instanceof File)) {
-            return [false, 400, { success: false, message: 'img_banner must be a File object' }];
+            return [400, { success: false, message: 'img_banner must be a File object' }];
         }
 
         const file = icon
@@ -22,13 +26,13 @@ export async function addSkillToTopic(topic_id: number, name: FormDataEntryValue
         
         //check icon is svg
         if (file.type !== 'image/svg+xml') {
-            return [false, 400,{success: false, message: 'icon must be svg'}]
+            return [400,{success: false, message: 'icon must be svg'}]
         }
 
         //get icon extension
         const icon_ext = file.name.split('.')[-1]
         if (icon_ext !== 'svg') {
-            return [false, 400,{success: false, message: 'icon must be svg'}]
+            return [400,{success: false, message: 'icon must be svg'}]
         }
 
         const file_name = `${uuidv7()}.${icon_ext}`
@@ -49,7 +53,7 @@ export async function addSkillToTopic(topic_id: number, name: FormDataEntryValue
             }
         })
 
-        return [false, 400,{success: true, message: 'skill topic added successfully', data: result}]
+        return [200,{success: true, message: 'skill topic added successfully', data: result}]
     }catch (error) {
         throw error
     }
--- src/app/api/skill/topic/[id]/route.ts
+++ src/app/api/skill/topic/[id]/route.ts
@@ -3,17 +3,13 @@ import { NextResponse , NextRequest} from 'next/server'
 
 export async function POST(Request: NextRequest, {params}: {params: {id: number}}) {
     try {
+
         const FromData = await Request.formData()
         const name = FromData.get('name')
         const icon = FromData.get('icon')
 
-
-        const data = await addSkillToTopic(params.id, name, icon)
-        if (!data[0]){
-            return NextResponse.json(data[2], {status: data[1]})
-        }
-        
-        return NextResponse.json(data[2], {status: data[1]})
+        const [status, message] = await addSkillToTopic(params.id, name, icon)
+        return NextResponse.json( message, {status: status})
     } catch (error) {
         return NextResponse.json({success: false,message: 'failed to add contact data',error: error}, {status: 500})
     }
