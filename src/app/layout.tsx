852585369a1d15e15cdb4b768d9fb091246ec78c
korarit <krtza1@gmail.com>
2024-11-29 23:05:11 +0700

    fix next build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,10 @@ import { Fira_Code } from 'next/font/google'
 
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
-const FiraCode = Fira_Code()
+const FiraCode = Fira_Code({
+  subsets: ['latin', 'cyrillic-ext', 'greek', 'latin-ext','cyrillic', 'greek-ext', ],
+  weight:['300', '400', '500', '600', '700']
+})
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
--- src/components/FrontawesomeIcon.tsx
+++ src/components/FrontawesomeIcon.tsx
@@ -6,11 +6,11 @@ import { library , config } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
 
-library.add(fab, fas, far)
+library.add(fab, fas, far);
 config.autoAddCss = false
 
-import { IconProp } from '@fortawesome/fontawesome-svg-core'
 
 export default function FrontawesomeIcon({className, icon}: Readonly<{className: string;icon: string;}>) {
     return (
