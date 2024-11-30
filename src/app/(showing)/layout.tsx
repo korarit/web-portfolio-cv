216de5415a02423e6d076501b9be76b16ead9781
korarit <krtza1@gmail.com>
2024-11-30 18:40:59 +0700

    add favicon and preview web

--- src/app/(showing)/layout.tsx
+++ src/app/(showing)/layout.tsx
@@ -14,12 +14,49 @@ export const metadata: Metadata = {
         type: 'website',
         url: 'https://cv.korarit.website/projects',
         title: 'Korarit - Portfolio',
+        images: {
+            url: 'https://pub-f562933a06224aeda971ebad86c0aea5.r2.dev/preview/preview-home.png',
+            width: 1440,
+            height: 1024,
+            alt: 'Korarit - Portfolio'
+        },
         description: 'Korarit\'s Portfolio Website - Hello',
     },
     authors: [{
         name: 'Korarit Saengthong',
         url: 'https://cv.korarit.website'
     }],
+    twitter: {
+        title: 'Korarit - Portfolio',
+        description: 'Korarit\'s Portfolio Website - Hello',
+        creator: '@thestepkla',
+        images: {
+            url: 'https://pub-f562933a06224aeda971ebad86c0aea5.r2.dev/preview/preview-home.png',
+            alt: 'Korarit - Portfolio'
+        }
+    },
+    icons:[
+        {
+            url: '/icon/icon-16.png',
+            sizes: '16x16',
+            type: 'image/png'
+        },
+        {
+            url: '/icon/icon-32.png',
+            sizes: '32x32',
+            type: 'image/png'
+        },
+        {
+            url: '/icon/icon-48.png',
+            sizes: '48x48',
+            type: 'image/png'
+        },
+        {
+            url: '/icon/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+        }
+    ]
 }
 
 export default function Layout({children}: Readonly<{children: React.ReactNode;}>) {
