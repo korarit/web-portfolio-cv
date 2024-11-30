a885407e28e6f6ae97f667db8d7384eea2cdd2b5
korarit <krtza1@gmail.com>
2024-11-30 18:40:45 +0700

    add favicon forwebsite

--- src/app/(showing)/skill/layout.tsx
+++ src/app/(showing)/skill/layout.tsx
@@ -21,7 +21,29 @@ export const metadata: Metadata = {
         title: 'Korarit - Skills',
         description: 'Korarit\'s Portfolio Website - My List of Tech Skills',
         creator: '@thestepkla',
-    }
+    },
+    icons:[
+        {
+            url: '/icon/icon-16.png',
+            sizes: '16x16',
+            type: 'image/png'
+        },
+        {
+            url: '/icon/icon-32.png',
+            sizes: '32x32',
+            type: 'image/png'
+        },
+        {
+            url: '/icon/icon-48.png',
+            sizes: '48x48',
+            type: 'image/png'
+        },
+        {
+            url: '/icon/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+        }
+    ]
 }
 
 export default function Layout({children}: Readonly<{children: React.ReactNode;}>) {
